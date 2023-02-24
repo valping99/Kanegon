b89e4723797da9a398f434d3f6b3958349df7f69
datdiep754129 <dat.diep@indigames.net>
2023-02-24 15:29:08 +0700

    + Add font + Add UI Coupon (mockup)

--- Assets/Scripts/UI/Result/Coupon.cs
+++ Assets/Scripts/UI/Result/Coupon.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 namespace Kanegon
 {
@@ -9,6 +11,9 @@ namespace Kanegon
         [SerializeField] private GameObject openBox;
         [SerializeField] private GameObject closeBox;
         [SerializeField] public float pointToGetBox;
+        [SerializeField] private TextMeshProUGUI _TextMessageCloseBox;
+        [SerializeField] private TextMeshProUGUI _TextMessageOpenBox;
+        [SerializeField] private TextMeshProUGUI _TextMessageLinkResult;
 
         public void CheckMysteryBox(float point)
         {
@@ -17,6 +22,14 @@ namespace Kanegon
 
             if (point >= pointToGetBox) openBox.gameObject.SetActive(true);
             else closeBox.gameObject.SetActive(true);
+            SetTextMessage();
+        }
+
+        private void SetTextMessage()
+        {
+            _TextMessageCloseBox.text = $"￥{pointToGetBox}以上を達成すると\n素敵な情報やクーポンコードが\n手に入れるよ！";
+            _TextMessageOpenBox.text = $"￥{pointToGetBox}以上を達成しました！\nクーポンコードをプレゼント！";
+            _TextMessageLinkResult.text = $"￥{pointToGetBox}以上を達成しました！\nお得な情報をご案内。\n連携ページで確認しよう！";
         }
     }
 }
